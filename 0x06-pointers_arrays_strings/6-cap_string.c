21fc7348762741424f2263aed90c5bffcdb932bb
FastionistaQueen <floressdiana47@gmail.com>
2023-10-19 23:33:07 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,30 +7,36 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-	char separators[] = " \t\n,;.!?\"(){}";
+	int i, j;
+int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (str[i] != '\0')
-	{
-		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 32; /* Convert to uppercase */
-		}
-
-		for (int j = 0; separators[j] != '\0'; j++)
-		{
-			if (str[i] == separators[j])
-			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				{
-					str[i + 1] = str[i + 1] - 32; /* Convert to uppercase */
-				}
-			}
-		}
-		i++;
-	}
-	return (str);
+i = 0;
+while (s[i] != '\0')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+if (i == 0)
+s[i] = s[i] - 32;
+else
+{
+for (j = 0; j <= 12; j++)
+{
+if (separators[j] == s[i - 1])
+s[i] = s[i] - 32;
 }
+}
+}
+i++;
+}
+return (s);
+}
+
+
+
+
+
+
+
 
 
 
