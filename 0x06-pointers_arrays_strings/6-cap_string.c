a11a5b9ef606d915add296d0b532a9d1bc7563b2
FastionistaQueen <floressdiana47@gmail.com>
2023-10-19 23:16:22 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: The capitalized string.
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32; /* Convert to uppercase */
+		}
+
+		for (int j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] = str[i + 1] - 32; /* Convert to uppercase */
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
+}
+
+
+
+
+
+
