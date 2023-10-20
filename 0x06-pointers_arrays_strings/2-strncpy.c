9e90e23e5351117fe233ef88b11404a61ac5d988
FastionistaQueen <floressdiana47@gmail.com>
2023-10-19 23:38:18 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,60 +1,27 @@
+#include <stdio.h>
 #include "main.h"
 /**
-  * _strncpy - The main function of the code
-  *
-  * @src: Function parameter
-  *
-  * @dest: Function parameter
-  *
-  * @n: Function parameter
-  *
-  * Return: dest
-  */
+ * *_strncpy - find the length of a string
+ * @dest: pointer to the string
+ * @src: pointer
+ * @n: int
+ * Return: destination
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int k = 0;
+int i;
 
-	while (k < n && src[k] != '\0')
-	{
-	dest[k] = src[k];
-	k++;
-	}
-	while  (k < n)
-	{
-	dest[k] = '\0';
-	k++;
-	}
-	return (dest);
-	}
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+
+for ( ; i < n; i++)
+	dest[i] = '\0';
+
+return (dest);
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-						        
 
 
 
